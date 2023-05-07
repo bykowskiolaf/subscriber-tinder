aaff4a3e438ae90afc78f17c0b5e83fd0b767f5c
bykowskiolaf <olafbykowski1@gmail.com>
2023-05-07 22:24:35 +0200

    added tailwind and handlebars

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/*.{hbs}',
+    './views/**/*.{hbs}',
+    'views/layouts/main.hbs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
-import { config } from "dotenv";
+require('dotenv').config({ path: `./config/.env` });
 import express from "express";
-import authRoutes from "../routes/auth";
 import passport from "passport";
+import { engine } from 'express-handlebars';
+import authRoutes from "../routes/auth";
+import indexRoutes from "../routes/index";
 const morgan = require("morgan");
-const connectDB = require(`../config/db`); 
-const exphbs = require("express-handlebars");
+const connectDB = require(`../config/db`);
 
 const PORT = process.env.PORT || 5000;
 const ENV_MODE = process.env.NODE_ENV || "development";
-
-// load config from config file
-config({ path: `./config/.env` });
 
 // connect to DB
 connectDB();
@@ -26,21 +24,20 @@ if (ENV_MODE === "development") {
 app.use(express.static("public"));
 
 // Handlebars
-app.engine('.hbs',  exphbs( {extname: '.hbs'} ));
-app.set('view engine', '.hbs');
+app.engine(".hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 app.use(passport.initialize());
 
 try {
-  app.listen(PORT, () => console.debug(`Running in ${ENV_MODE} mode on port ${PORT}.`));
+  app.listen(PORT, () =>
+    console.debug(`Running in ${ENV_MODE} mode on port ${PORT}.`)
+  );
 } catch (error) {
   console.log(error);
   process.exit(1);
 }
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// routers 
+// routers
 app.use("/api/auth", authRoutes);
+app.use("/", indexRoutes);
